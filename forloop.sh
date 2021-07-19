1eb03afb3709f3ac69ed7a25cf66b2a80f79405e
MuthyalaSudheer <sudheerlikeu@gmail.com>
2021-07-19 17:15:11 +0530

    Create forloop.sh
    example-1

---
+++ forloop.sh
@@ -0,0 +1,3 @@
+#!/bin/bash 
+# example-1
+$ for i in frontend mongodb catalogue user cart redis mysql shipping rabbitmq payment ;do  git clone https://sudheerlikeu@dev.azure.com/sudheerlikeu/D54/_git/$i ;done
