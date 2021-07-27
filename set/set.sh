cbf2b2ad6a76785ee25ddbfa130c3afce2b54536
MuthyalaSudheer <sudheerlikeu@gmail.com>
2021-07-27 16:07:50 +0530

    Update set.sh
    demo on set command

--- set/set.sh
+++ set/set.sh
@@ -1,21 +1,14 @@
 #!/bin/bash
 # Demo on how to work with "SET" command
 
-
-
-URL="curl -s https://nodejs.org/en/download/ | xargs -n1 | grep 'linux-x64.tar.xz' | sed -e 's|=| |g' -e 's|>| |g' | xargs -n1| grep ^https"
-FILENAME=$(echo $URL | awk -F '/' '{print $NF}')
-FOLDER_NAME=$(echo $FILENAME | sed -e 's|.tar.xz| |')
-
-curl -s -o /tmp/$FILENAME $URL
-
-tar -xf /tmp/$FILENAME
-
-mv $FOLDER_NAME nodejs
-ln -s /opt/nodejs/bin/node /bin/node || true
-ln -s /opt/nodejs/bin/npm /bin/npm || true
-ln -s /opt/nodejs/bin/npx /bin/npx || true
+set `date`
+echo "Today is $1"
+echo "Month is $2"
+echo "Date is $3"
+echo "Time is H:M:S $4"
+echo "Year is $6"
 
 
 
-cd /opt 
+
+
