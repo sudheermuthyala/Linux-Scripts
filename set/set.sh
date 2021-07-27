c5a38de5e9df0ed674a613de63d4580c25be341f
MuthyalaSudheer <sudheerlikeu@gmail.com>
2021-07-27 15:33:06 +0530

    Update set.sh

--- set/set.sh
+++ set/set.sh
@@ -5,6 +5,17 @@
 
 URL="curl -s https://nodejs.org/en/download/ | xargs -n1 | grep 'linux-x64.tar.xz' | sed -e 's|=| |g' -e 's|>| |g' | xargs -n1| grep ^https"
 FILENAME=$(echo $URL | awk -F '/' '{print $NF}')
+FOLDER_NAME=$(echo $FILENAME | sed -e 's|.tar.xz| |')
+
+curl -s -o /tmp/$FILENAME $URL
+
+tar -xf /tmp/$FILENAME
+
+mv $FOLDER_NAME nodejs
+ln -s /opt/nodejs/bin/node /bin/node || true
+ln -s /opt/nodejs/bin/npm /bin/npm || true
+ln -s /opt/nodejs/bin/npx /bin/npx || true
 
 
-https://nodejs.org/dist/v14.17.3/node-v14.17.3-linux-x64.tar.xz
+
+cd /opt 
